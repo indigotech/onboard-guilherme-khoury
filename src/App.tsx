31f45ae993b15b7736f2104e4cb20536512d376e
Taqtile <taqtile@Brewdog.local>
2021-09-09 09:16:11 -0300

    Break tags retiradas para a utilização de marginBottom nos inputs.

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,27 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+
         <h1>Bem Vindo(a) à Taqtile</h1>
+
         <label>
           E-mail:
         </label>
-        <input type="email"/>
-        <br/>
+        <input type="email" style={{marginBottom:'1em'}}/>
+
         <label>
           Senha:
         </label>
-        <input type="password"/>
-        <br/>
+        <input type="password" style={{marginBottom:'3em'}}/>
+
         <button type="submit">Entrar</button>
+
       </header>
     </div>
   );
 }
 
+
 export default App;
+
+
