bd4d2bc46b036c655d01a99a21b0c17bb9beb864
Guilherme Khoury <gui.khoury@usp.br>
2021-09-16 16:23:14 -0300

    Problemas com o index

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ExchangeRates from './Index'
 
 function App() {
 
@@ -65,9 +66,11 @@ function App() {
           </div>
           
         </form>
-
       </header>
+      <ExchangeRates/>
     </div>
+
+
   );
 }
 
