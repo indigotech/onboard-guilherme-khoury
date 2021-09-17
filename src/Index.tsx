191d50910988fbe2074a49640e3e76756699fa77
Guilherme Khoury <gui.khoury@usp.br>
2021-09-17 19:21:42 -0300

    Integração com o server.

--- src/Index.tsx
+++ src/Index.tsx
@@ -3,56 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './ReportWebVitals';
-import {
+import { ApolloProvider,
   ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql
-} from "@apollo/client";
+  InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  uri: 'https://tq-template-server-sample.herokuapp.com/graphql',
   cache: new InMemoryCache()
 });
-
-client
-  .query({
-    query: gql`
-      query GetRates {
-        rates(currency: "USD") {
-          currency
-        }
-      }
-    `
-  })
-  .then(result => console.log(result));
-
-  const EXCHANGE_RATES = gql`
-  query GetExchangeRates {
-    rates(currency: "USD") {
-      currency
-      rate
-    }
-  }
-`;
-
-export default function ExchangeRates() {
-
-  const { loading, error, data } = useQuery(EXCHANGE_RATES);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-  return data.rates.map(({ currency, rate }) => (
-    <div key={currency}>
-      <p>
-        {currency}: {rate}
-      </p>
-    </div>
-  ));
-}
-
 
 ReactDOM.render(
   <React.StrictMode>
